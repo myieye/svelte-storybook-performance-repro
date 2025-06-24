293fc5dda74770163150ca384b8f455545d34e8f
Tim Haasdyk <tim_haasdyk@sil.org>
2025-06-24 16:46:40 +0200

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import {defineConfig} from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import path from 'path';
+import {sveltekit} from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
 	test: {
 		projects: [
 			{
