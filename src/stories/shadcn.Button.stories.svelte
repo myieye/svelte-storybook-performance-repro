4b05888d4f2c6ed18f3b7625c5642f0df9870664
Tim Haasdyk <tim_haasdyk@sil.org>
2025-08-11 11:13:01 +0200

    Prepare performance repro

---
+++ src/stories/shadcn.Button.stories.svelte
@@ -0,0 +1,20 @@
+<script module>
+	import Button from '$lib/components/ui/button/button.svelte';
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: 'Shadcn/Button',
+    component: Button,
+    tags: ['autodocs'],
+  });
+</script>
+
+<Button title="Test Button" class="bg-blue-500 text-white p-2 rounded">
+  Test button
+</Button>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Default">
+  Test button
+</Story>
